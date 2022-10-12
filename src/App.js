5cd7dbf3a58d579be9f883fa77b3f6d259b1247e
matanzh20000 <matanzh20000@gmail.com>
2022-10-12 19:46:56 +0300

    Fourth commit

--- src/App.js
+++ src/App.js
@@ -3,64 +3,30 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useEffect, Fragment } from "react";
-import { uiActions } from "./components/store/ui-slice";
+import { sendCartData, fetchCartData } from "./components/store/cart-actions";
 import Notification from "./components/UI/Notification";
 
-let isInitial = (true)
+let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
-  const dispatch = useDispatch();
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "sending...",
-          message: "Sending data to Backend",
-        })
-      );
-      const response = await fetch(
-        "https://http-redux-18858-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
-        { method: "PUT", body: JSON.stringify(cart) }
-      );
+    dispatch(fetchCartData());
+  }, [dispatch]);
 
-      if (!response.ok) {
-        dispatch(
-          uiActions.showNotification({
-            status: "error",
-            title: "failed",
-            message: "Sending data to Backend Failed",
-          })
-        );
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Sent data Successfully.",
-          message: "Sent data to Backend Successfully",
-        })
-      );
-    };
-
-    if(isInitial) {
+  useEffect(() => {
+    if (isInitial) {
       isInitial = false;
       return;
     }
 
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "failed",
-          message: "Sending data to Backend Failed",
-        })
-      );
-    });
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
   }, [cart, dispatch]);
 
   return (
